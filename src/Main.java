f8fc4be4b32da6812d5f91d36dcb9b76da192a8a
TibereLN <tibere55190@gmail.com>
2025-01-18 10:44:05 +0100

    modif

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-import KNN.*;
-import MLP.*;
+import KNN.AlgoMLP;
+import KNN.Donnees;
+import KNN.MNISTLoader;
+import MLP.Hyperbolic;
+import MLP.Sigmoid;
+import MLP.TransferFunction;
 
 import java.io.IOException;
 
@@ -9,30 +13,23 @@ public class Main {
             System.out.println("Chargement des données d'entraînements...");
             Donnees donneesTrain = MNISTLoader.loadData("images/train-images.idx3-ubyte", "images/train-labels.idx1-ubyte");
 
-            int[] layers = {200, 50, 10, 1};
-            double tauxApprentissage = 0.01;
+            int[] layers = {210, 70, 10, 1};
+            double tauxApprentissage = 1;
             TransferFunction fonctionActivation = new Sigmoid();
             TransferFunction fonctionActivation2 = new Hyperbolic();
-            AlgoMLP algoMLP = new AlgoMLP(donneesTrain, layers, tauxApprentissage, fonctionActivation);
+            double erreur_cible = 0.00001;
+            int max_iterations = 1000;
 
-            System.out.println("Entraîenement du MLP...");
-            double erreur_cible = 0.0001;
-            int max_iterations = 100;
+            AlgoMLP algoMLP = new AlgoMLP(donneesTrain, layers, tauxApprentissage, fonctionActivation);
+            algoMLP.mlp.setLearningRate(1);
+            System.out.println("START : Etiquettes : "+ donneesTrain.getImagette(0).getLabel() + " "+ donneesTrain.getImagette(1).getLabel() +" Prédiction : "+algoMLP.mlp.execute(donneesTrain.getImagette(0).getFlatPixels())+" "+algoMLP.mlp.execute(donneesTrain.getImagette(1).getFlatPixels()));
 
             for (int i = 0; i < 10; i++) {
                 double erreur = algoMLP.train(erreur_cible, max_iterations);
-                System.out.println("Itération " + i + " Erreur : "+erreur+" Précision : "+ Statistique.calculerPrecision(algoMLP, donneesTrain));
+                //System.out.println("Itération " + i + " Erreur : "+erreur+" Précision : "+ Statistique.calculerPrecision(algoMLP, donneesTrain));
+                System.out.println("Itération " + i + " Erreur : "+erreur+ " Etiquettes : "+ donneesTrain.getImagette(0).getLabel() + " "+ donneesTrain.getImagette(1).getLabel() +" Prédiction : "+algoMLP.mlp.execute(donneesTrain.getImagette(0).getFlatPixels())+" "+algoMLP.mlp.execute(donneesTrain.getImagette(1).getFlatPixels()));
+                algoMLP.mlp.setLearningRate(Math.min(0.01, erreur));
             }
-
-
-//            System.out.println("Chargement des données de tests...");
-//            Donnees donneesTest = MNISTLoader.loadData("images/t10k-images.idx3-ubyte", "images/t10k-labels.idx1-ubyte");
-//
-//            System.out.println("Calcul de la précision...");
-//            double precision = Statistique.calculerPrecision(algoMLP, donneesTrain);
-//            System.out.println("Precision MLP : " + precision*100 + "% sur les données d'entraînements");
-//            double precision2 = Statistique.calculerPrecision(algoMLP, donneesTest);
-//            System.out.println("Precision MLP : " + precision2*100 + "% sur les données de test");
         }
         catch (IOException e) {
             e.printStackTrace();
